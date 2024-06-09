911a21667187e3722630a9e1a1229f8575e863aa
EdwinVBN <Katuku.edwin@gmail.com>
2024-06-09 22:50:38 +0200

    push

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
-import { animals } from './animals.json';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, ScrollView, Pressable, Image, Alert, Button  } from 'react-native';
+import { cars } from './car.json';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+const naam = 'Admin';
+const wachtwoord = 'password';
+
+function LoginScreen({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (username === naam && password === wachtwoord) {
+      navigation.navigate('CarGallery');
+    } else {
+      Alert.alert('Incorrect', 'Username or password is incorrect');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        placeholder="Username"
+      />
+      <TextInput
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <Button title='Login' onPress={handleLogin} />
+    </View>
+  );
+}
+
 function CarDetailsScreen({ route }) {
-  const { car } = route.params;
+  const { auto } = route.params;
   return (
     <View style={styles.detialsContainer}>
-      <Image source={{ uri: car.imageSource }} style={styles.detialsImage} />
+      <Image source={{ uri: auto.image }} style={styles.detialsImage} />
       <View style={styles.textContainer}>
-        <Text style={styles.textTitel}>{car.name}</Text>
-        <Text style={styles.detialsText}>{car.description}</Text>
+        <Text style={styles.textTitel}>{auto.merk}</Text>
+        <Text style={styles.detialsText}>{auto.model} - {auto.bouwjaar}</Text>
+        <Text style={styles.detialsText}>{auto.beschrijving}</Text>
       </View>
     </View>
   );
@@ -33,28 +68,28 @@ export default function App() {
           },
         }}
       >
-        <Stack.Screen name="AnimalGallery" component={CarGalleryScreen} options={{ title: 'Animal Gallery' }} />
-        <Stack.Screen name="AnimalDetails" component={CarDetailsScreen} options={{ title: 'Animal Details' }} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Login' }} />
+        <Stack.Screen name="CarGallery" component={CarGalleryScreen} options={{ title: 'Car Gallery' }} />
+        <Stack.Screen name="CarDetails" component={CarDetailsScreen} options={{ title: 'Car Details' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 function CarGalleryScreen({ navigation }) {
-  const autoInfo = (animal) => {
-    navigation.navigate('AnimalDetails', { animal });
+  const autoInfo = (auto) => {
+    navigation.navigate('CarDetails', { auto });
   };
   
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollview}>
-        <View style={styles.animalContainer}>
-          
+        <View style={styles.carContainer}>
           {
-            animals.map((animal) => (
-              <TouchableOpacity key={animal.name} onPress={() => autoInfo(animal)}>
-                <Image source={{uri: animal.imageSource}} style={styles.animalImage} />
-              </TouchableOpacity>
+            cars.map((auto) => (
+              <Pressable key={auto.id} onPress={() => autoInfo(auto)}>
+                <Image source={{uri: auto.image}} style={styles.carImage} />
+              </Pressable>
             ))
           }
         </View>
@@ -89,13 +124,13 @@ const styles = StyleSheet.create({
     padding: 16,
     marginTop: 50,
   },
-  animalContainer: {
+  carContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
     paddingHorizontal: 20,
   },
-  animalImage: {
+  carImage: {
     height: 150,
     width: 150,
     marginBottom: 25,
